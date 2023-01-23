e64794c675caa116ca3b5098d2b8dd4a809d8c9c
Rannael Santiago <74615002+rannaelsant@users.noreply.github.com>
2023-01-23 11:17:53 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=200, pady=200)
+
+canvas = Canvas()
+mypass_image = PhotoImage(file="logo.png")
+
+window.mainloop()
+
